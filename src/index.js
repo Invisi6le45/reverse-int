ae35b6b1b98e7431c25543d4f12b2148fd3fcc58
Nikolai <113165334+Invisi6le45@users.noreply.github.com>
2024-07-29 08:40:33 +0300

    fix problem

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 module.exports = function reverse (n) {
- let reverNumber = Array;
- let result = '';
- let midleNumber;
- let negativeNumber;
- if (n < 0) {
-   negativeNumber = 1;
- } else {
-   negativeNumber = 1;
-   };
+  let reverNumber = Array;
+  let result = '';
+  let midleNumber;
+  let negativeNumber;
+
+  if (n < 0) {
+    negativeNumber = 1;
+  } else {
+    negativeNumber = 1;
+    };
+
   midleNumber = Math.abs (n)
- let numberLeter = midleNumber.toString();  
- for ( i = numberLeter.length; i > 0; i--) {
-   reverNumber[i-1] = (`${numberLeter[i-1]}`);
-   result = result + reverNumber[i-1]
- }
- return Number(result) * negativeNumber;
+  let numberLeter = midleNumber.toString();  
+  for ( i = numberLeter.length; i > 0; i--) {
+    reverNumber[i-1] = (`${numberLeter[i-1]}`);
+    result = result + reverNumber[i-1]
+  }
+  return Number(result) * negativeNumber;
 }
