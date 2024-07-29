12c27d584a1f780cd5d7aa74f03e9b2578868272
Nikolai <113165334+Invisi6le45@users.noreply.github.com>
2024-07-29 08:38:33 +0300

    add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+ let reverNumber = Array;
+ let result = '';
+ let midleNumber;
+ let negativeNumber;
+ if (n < 0) {
+   negativeNumber = 1;
+ } else {
+   negativeNumber = 1;
+   };
+  midleNumber = Math.abs (n)
+ let numberLeter = midleNumber.toString();  
+ for ( i = numberLeter.length; i > 0; i--) {
+   reverNumber[i-1] = (`${numberLeter[i-1]}`);
+   result = result + reverNumber[i-1]
+ }
+ return Number(result) * negativeNumber;
 }
